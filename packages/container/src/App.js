77008843b8ea809fe8c7a168b7451b5808de5c1c
Anderson Oliveira Ferreira <aoferreira@ecad.org.br>
2022-09-05 21:47:12 -0300

    inclusão do generateClassName para resolver o problema de classe com o mesmo nome

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StylesProvider, generateClassName } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+const generateClassName = generateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </Router>
   );
 };
